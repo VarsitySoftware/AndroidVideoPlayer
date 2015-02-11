08f3ffab5e9f8ced1b90dc27c448836feb9ae67f
VarsitySoftware <github@varsitysoftware.com>
2015-02-10 21:51:01 -0500

    Update AndroidVideoPlayer.java

--- src/android/AndroidVideoPlayer.java
+++ src/android/AndroidVideoPlayer.java
@@ -30,6 +30,7 @@ import org.apache.cordova.PluginResult;
 public class AndroidVideoPlayer extends CordovaPlugin {
     private static final String YOU_TUBE = "youtube.com";
     private static final String ASSETS = "file:///android_asset/";
+    private static final String CONTENT = "content://";
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
@@ -95,7 +96,22 @@ public class AndroidVideoPlayer extends CordovaPlugin {
             // Display video player
             intent = new Intent(Intent.ACTION_VIEW);
             intent.setDataAndType(uri, "video/*");
-        } else {
+        } 
+        else if(url.contains(CONTENT)) 
+        {
+            Uri uriMediaURL = Uri.parse(url);
+            String strPath = getPath(context, uriMediaURL);
+            Log.i("CC", "XXXXXstrPath = " + strPath);
+            
+            uri = Uri.parse("file://" + strPath);
+            
+            // Display video player
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setDataAndType(uri, "video/*");
+            
+            Log.i("CC", "XXXXXstrPathZZZZZZ");
+        }
+            else {
             // Display video player
             intent = new Intent(Intent.ACTION_VIEW);
             intent.setDataAndType(uri, "video/*");
@@ -130,4 +146,150 @@ public class AndroidVideoPlayer extends CordovaPlugin {
             return false;
         }
     }
+    
+    /**
+    * Get a file path from a Uri. This will get the the path for Storage Access
+    * Framework Documents, as well as the _data field for the MediaStore and
+    * other file-based ContentProviders.
+    *
+    * @param context The context.
+    * @param uri The Uri to query.
+    * @author paulburke
+    */
+    public static String getPath(final Context context, final Uri uri) 
+    {
+    	final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+	
+	    // DocumentProvider
+	    if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+	        // ExternalStorageProvider
+	        if (isExternalStorageDocument(uri)) {
+	            final String docId = DocumentsContract.getDocumentId(uri);
+	            final String[] split = docId.split(":");
+	            final String type = split[0];
+	
+	            if ("primary".equalsIgnoreCase(type)) {
+	                return Environment.getExternalStorageDirectory() + "/" + split[1];
+	            }
+	
+	            // TODO handle non-primary volumes
+	        }
+	        // DownloadsProvider
+	        else if (isDownloadsDocument(uri)) {
+	
+	            final String id = DocumentsContract.getDocumentId(uri);
+	            final Uri contentUri = ContentUris.withAppendedId(
+	                    Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+	
+	            return getDataColumn(context, contentUri, null, null);
+	        }
+	        // GoogleDriveProvider
+	        else if (isGoogleDriveDocument(uri)) {
+	
+	            //final String id = DocumentsContract.getDocumentId(uri);
+	            //final Uri contentUri = ContentUris.withAppendedId(
+	            //        Uri.parse("content://com.google.android.apps.docs.storage"), id);
+	
+	            return uri.getPath();
+	        }
+	        // MediaProvider
+	        else if (isMediaDocument(uri)) {
+	            final String docId = DocumentsContract.getDocumentId(uri);
+	            final String[] split = docId.split(":");
+	            final String type = split[0];
+	
+	            Uri contentUri = null;
+	            if ("image".equals(type)) {
+	                contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+	            } else if ("video".equals(type)) {
+	                contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+	            } else if ("audio".equals(type)) {
+	                contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+	            }
+	
+	            final String selection = "_id=?";
+	            final String[] selectionArgs = new String[] {
+	                    split[1]
+	            };
+	
+	            return getDataColumn(context, contentUri, selection, selectionArgs);
+	        }
+	    }
+	    // MediaStore (and general)
+	    else if ("content".equalsIgnoreCase(uri.getScheme())) {
+	        return getDataColumn(context, uri, null, null);
+	    }
+	    // File
+	    else if ("file".equalsIgnoreCase(uri.getScheme())) {
+	        return uri.getPath();
+	    }
+	
+	    return null;
+	}
+
+	/**
+	 * Get the value of the data column for this Uri. This is useful for
+	 * MediaStore Uris, and other file-based ContentProviders.
+	 *
+	 * @param context The context.
+	 * @param uri The Uri to query.
+	 * @param selection (Optional) Filter used in the query.
+	 * @param selectionArgs (Optional) Selection arguments used in the query.
+	 * @return The value of the _data column, which is typically a file path.
+	 */
+	public static String getDataColumn(Context context, Uri uri, String selection,
+	        String[] selectionArgs) {
+	
+	    Cursor cursor = null;
+	    final String column = "_data";
+	    final String[] projection = {
+	            column
+	    };
+	
+	    try {
+	        cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+	                null);
+	        if (cursor != null && cursor.moveToFirst()) {
+	            final int column_index = cursor.getColumnIndexOrThrow(column);
+	            return cursor.getString(column_index);
+	        }
+	    } finally {
+	        if (cursor != null)
+	            cursor.close();
+	    }
+	    return null;
+	}
+	
+	
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is ExternalStorageProvider.
+	 */
+	public static boolean isExternalStorageDocument(Uri uri) {
+	    return "com.android.externalstorage.documents".equals(uri.getAuthority());
+	}
+	
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is DownloadsProvider.
+	 */
+	public static boolean isDownloadsDocument(Uri uri) {
+	    return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+	}
+	
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is MediaProvider.
+	 */
+	public static boolean isMediaDocument(Uri uri) {
+	    return "com.android.providers.media.documents".equals(uri.getAuthority());
+	}
+	
+	/**
+	 * @param uri The Uri to check.
+	 * @return Whether the Uri authority is Google Docs Provider.
+	 */
+	public static boolean isGoogleDriveDocument(Uri uri) {
+	    return "com.google.android.apps.docs.storage".equals(uri.getAuthority());
+	}
 }
