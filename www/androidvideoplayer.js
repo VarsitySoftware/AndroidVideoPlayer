8d57df7f549c1eee9a01df68cd45c47623fde95d
Varsity Software <github@varsitysoftware.com>
2018-01-18 18:03:17 -0500

    Delete androidvideoplayer.js

--- www/androidvideoplayer.js
+++
@@ -1,1 +0,0 @@
-window.getAndroidMetaData = function (strFilePath, strFileType, onStart, onFail) { cordova.exec(onStart, onFail, "AndroidMediaMetaDataRetriever", "getAndroidMetadata", [strFilePath, strFileType]); };
