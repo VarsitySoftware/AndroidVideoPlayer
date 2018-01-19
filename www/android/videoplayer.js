430da582b700ff39ce957b32df1218fc30385c0a
Varsity Software <github@varsitysoftware.com>
2018-01-18 23:43:50 -0500

    Update videoplayer.js

--- www/android/videoplayer.js
+++ www/android/videoplayer.js
@@ -1,1 +1,1 @@
-window.playAndroidVideo = function (strURL, onStart, onFail) { console.log("XXX - WTF"); cordova.exec(onStart, onFail, "VideoPlayer", "play", [strURL]); };
+window.playAndroidVideo = function (strURL, onStart, onFail) { cordova.exec(onStart, onFail, "VideoPlayer", "play", [strURL]); };
